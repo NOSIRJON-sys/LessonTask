150dc9407d29095c10f35c471a75f716194321af
user <user@WIN-6M77HE04LE6>
2025-02-08 17:21:15 +0500

    Shart operatorlari qo'shildi+1

--- ShartAperatorlari.py
+++ ShartAperatorlari.py
@@ -4,4 +4,9 @@ x=int(input("X="))
 if x<-2:
     y=math.sin(x)+math.sqrt(math.fabs(x+2))
 elif x>2:
-    y=math.pow(x+5,13)
+    y=math.pow(x+5,1/3)*math.tan(x/2)
+else:
+    y=x**2*(math.cos(math.pi))+math.log(x+6.78)
+print("y=",y)
+
+
